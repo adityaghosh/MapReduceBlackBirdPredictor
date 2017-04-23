0ad4d10e01b9d030b1485f2f0e46fdf5150fe2d4
Rohit Kumar <rohitkumar2506@gmail.com>
2017-04-23 03:28:29 -0400

    Added the Cross validation step

--- src/main/scala/com/birdpredictor/App.scala
+++ src/main/scala/com/birdpredictor/App.scala
@@ -15,6 +15,8 @@ import org.apache.spark.mllib.tree.model.RandomForestModel
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics;
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.catalyst.plans.logical.Repartition
+import java.text.Normalizer.Form
+import java.util.concurrent.ConcurrentHashMap.ForEachEntryTask
 
 object App {
   def main(args: Array[String]) {
@@ -23,14 +25,12 @@ object App {
     val output = args(1)
     //define local or cluster execution
     val exeType = args(2)
-    // by default running is cluster
+    val unlabeled = "unlabeled"
     val conf = new SparkConf()
-    var sc = new SparkContext("local[*]", "BirdPredictor", conf)
 
-    //val conf = new SparkConf().setAppName("SVMWithSGDExample")
-    if (exeType.equals("cluster")) {
-      sc = new SparkContext(conf)
-    }
+//    var sc = new SparkContext("local[*]", "BirdPredictor", conf)
+        var sc = new SparkContext(conf)
+    //
     var predata = sc.textFile(input + "/")
       .map(line => StringRecordParser.get(line))
       .filter(line => line.length() != 0 || line.split(",").length > 30)
@@ -46,41 +46,74 @@ object App {
     val training = splits(0).cache()
     val test = splits(1)
 
-    //    // Run training algorithm to build the model
-    //    val numIterations = 100
-    //    //    val model = SVMWithSGD.train(training, numIterations)
-    //
-    ////    Run training algorithm to build the model
-    //    val model = new LogisticRegressionWithLBFGS()
-    //      .setNumClasses(2)
-    //      .run(training)
-
-    // Train a RandomForest model.
-    // Empty categoricalFeaturesInfo indicates all features are continuous.
-    val numClasses = 2
-    val categoricalFeaturesInfo = Map[Int, Int]((11, 49), (12, 380))
-    val numTrees = 10 // Use more in practice.
+    //This is using RandomForest. Setting up the parameters for the Random Forest algorithm
+    val numClasses = 2  // binary classification
+    val categoricalFeaturesInfo = Map[Int, Int]((11, 49), (12, 380))  // features that are categorical and have been converted to continous 
+    val numTrees = 5 // Use more in practice. number of trees to spawn in the random Forest
     val featureSubsetStrategy = "auto" // Let the algorithm choose.
     val impurity = "gini"
-    val maxDepth = 15
-    val maxBins = 400
+    val maxDepth = 15  // Max Depth of the tree in Random forest
+    val maxBins = 400  // Maximum number of features 
 
-    val model = RandomForest.trainClassifier(training, numClasses, categoricalFeaturesInfo,
-      numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
-    //    val model = SVMWithSGD.train(training, numIterations)
+    val hyperTreeNum: List[Int] = List(5, 8, 11)
+    //    11, 14, 17, 20, 23, 26)
+    val hyperTreeDepth: List[Int] = List(12, 15, 18)
+    //    18, 21, 24, 27)
+    val i = 0
+    val j = 0
+    val indexes = for (
+      i <- hyperTreeNum;
+      j <- hyperTreeDepth
+    ) yield (i, j)
+
+    val models = for (index <- indexes) yield RandomForest.trainClassifier(training, numClasses, categoricalFeaturesInfo,
+      index._1, featureSubsetStrategy, impurity, index._2, maxBins)
+
+    //    val model = RandomForest.trainClassifier(data, numClasses, categoricalFeaturesInfo,
+    //      numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
     // Compute raw scores on the test set.
-    val scoreAndLabels = test.map { point =>
+
+    println("Starting to find the best model")
+
+    val scoreAndLableList = for (model <- models) yield test.map { point =>
       val score = model.predict(point.features)
-      println(score + " -- " + point.label + ":: features ::" + point.features)
       (score, point.label)
     }
+    
+    val errorList = scoreAndLableList.map(scorenLables => scorenLables.filter(r => r._1 != r._2).count.toDouble / test.count())
+    val sortedList = errorList.sorted
+    val minimum = sortedList(0)
 
-    val testErr = scoreAndLabels.filter(r => r._1 != r._2).count.toDouble / test.count()
-    println("Test Error = " + testErr)
-    //    model.save(sc, "target/tmp/scalaSVMWithSGDModel")
-    //    val sameModel = SVMModel.load(sc, "target/tmp/scalaSVMWithSGDModel")
-    // $example off$
+    val minIndex = errorList.indexOf(minimum)
+    val bestModel = models(minIndex)
+    
+    println("Found the best model")
+    println("best model is :: " + bestModel.toString())
+    println(errorList.toString() + "::" + minimum + "::" + minIndex)
+    println("best hyperParams are :: " + indexes(minIndex)._1 + " :: " + indexes(minIndex)._2)
 
+    /*
+    val finalModel = RandomForest.trainClassifier(data, numClasses, categoricalFeaturesInfo,
+      indexes(minIndex)._1, featureSubsetStrategy, impurity, indexes(minIndex)._2, maxBins)
+
+    
+    var unLableddata = sc.textFile(unlabeled + "/")
+      .map(line => StringRecordParser.get(line))
+      .filter(line => line.length() != 0 || line.split(",").length > 30)
+
+    unLableddata.saveAsTextFile(unlabeled)
+    val unlabeledPointData = MLUtils.loadLibSVMFile(sc, unlabeled)
+
+    val finalResult = unlabeledPointData.map { point =>
+      val score = finalModel.predict(point.features)
+      (score, 1)
+    }
+    
+    */
+    //    val testErr = scoreAndLabels.filter(r => r._1 != r._2).count.toDouble / test.count()
+
+    //    println("Test Error = " + testErr)
     sc.stop()
   }
+
 }
